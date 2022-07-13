754556ae761b9b2d6fc91a68b7c6ee76dd5d3ffc
matheusAFreitas <matheusalvesdefreitas@gmail.com>
2022-07-13 10:45:14 -0300

    feat: created list mock

--- crudAngular/src/app/courses/services/courses.service.ts
+++ crudAngular/src/app/courses/services/courses.service.ts
@@ -3,19 +3,20 @@ import { Injectable } from '@angular/core';
 
 import { Course } from '../model/course';
 
+import { tap, first } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesService {
   constructor(private httpClient: HttpClient) {}
 
-  list(): Course[] {
-    return [
-      {
-        _id: '1',
-        name: 'Angular',
-        category: 'frontEnd',
-      },
-    ];
+  private readonly API = '/assets/courses.json';
+
+  list() {
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      tap((courses) => console.log(courses))
+    );
   }
 }
