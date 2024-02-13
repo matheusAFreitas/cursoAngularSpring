6616f457976bb7a1276deffa96453cdf791add2c
matheusAFreitas <matheus.freitas@fcamara.com.br>
2024-02-13 15:00:31 -0300

    feat: added courses table

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ICourse } from '../model/ICourse';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrl: './courses.component.scss',
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+  coursesData: ICourse[] = [
+    {
+      _id: '1',
+      name: 'Angular',
+      category: 'front-end',
+    },
+  ];
+  displayedColumns: string[] = ['name', 'category'];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
